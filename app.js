559edeed55da667a49641a4da5583c960df455ea
ckbhatia <chetansain86@gmail.com>
2020-04-05 17:29:39 +0530

    fix: add dotenv in conditional

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
 // Connecting With DataBase
 mongoose.connect(
