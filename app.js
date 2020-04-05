d7474cd2f9ae81b979fb7b94fc2f0d6266e95cf4
ckbhatia <chetansain86@gmail.com>
2020-04-05 15:00:21 +0530

    feat: add mongoose and add orderRouter

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const orderRouter = require("./routes/order");
 
-var app = express();
+const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+require("dotenv").config();
+
+// Connecting With DataBase
+mongoose.connect(
+  process.env.DB_URL,
+  { useUnifiedTopology: true, useNewUrlParser: true },
+  function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("DB connected");
+    }
+  }
+);
+
+mongoose.set("useCreateIndex", true);
+
+app.use("/api/v1/order", cors(), orderRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 module.exports = app;
