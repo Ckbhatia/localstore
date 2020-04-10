90c9d6a8ac4a69f909ec07d653792425d39eb525
ckbhatia <chetansain86@gmail.com>
2020-04-10 15:47:25 +0530

    feat: add sendMsg

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,10 +1,15 @@
 const Order = require("../models/order");
+const sendMsg = require("../utils/msg");
 
 const orderController = {
   createOne: async (req, res) => {
     try {
       const order = await Order.create(req.body);
-      res.status(201).json({ msg: "Resource created!", data: order });
+      await res.status(201).json({ msg: "Resource created!", data: order });
+
+      const { orderId, cartTotal, number } = order;
+      // Send transaction
+      sendMsg(orderId, cartTotal, number);
     } catch (err) {
       console.error(err);
       res.status(400).json({ msg: "There's an error" });
